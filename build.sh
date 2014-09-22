203b8ce643c07099b11c6603edc7b8c2404150be
Olex Ponomarenko <tholex@gmail.com>
2014-09-22 16:57:58 -0400

    Make sure we don't screw with s3 for this test

--- build.sh
+++ build.sh
@@ -50,6 +50,7 @@ secret_key = $SECRET_KEY
 #### DEBUG
 ####
 echo "debug ==> $CURRICULA_FOLDER/${CODE}/${VERSION}/spliced.xml"
+exit 1
 
 #copy the curriculum.xml file if a new one was created
 if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]; then
