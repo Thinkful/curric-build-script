db336284559ec638d509c7d659b54a380ee75c2a
Olex Ponomarenko <tholex@gmail.com>
2014-09-22 18:38:34 -0400

    Reorganize, fix syntax issue.

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env bash
 
-if [ "$1" == "production" ]; then
+if [ "$1" == "production" ]
+then
   # production options
   echo "Setting 'production' options."
   export MASTER="true"
 fi
 
-if [ "$1" == "preview" ]; then
+if [ "$1" == "preview" ]
+then
   # preview options
   echo "Setting 'preview' options."
   export S3SERVER=${PREVIEW_S3SERVER}
@@ -21,9 +23,10 @@ cd thinkdown
 npm install
 npm link
 cd ..
-thinkdown --curric=${CURRICULA_FOLDER} || { echo "Thinkdown failed." ; exit 1 }
+thinkdown --curric=${CURRICULA_FOLDER}
 
-if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]; then
+if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]
+then
     echo "Thinkdown completed, spliced.xml exists."
 else
     echo "Thinkdown exited, but seems to have failed, spliced.xml not found."
@@ -31,7 +34,8 @@ else
 fi
 
 #commit changes to github if we're in the master branch
-if [ -n "$MASTER" ]; then
+if [ -n "$MASTER" ]
+then
     echo "Pushing structure.xml with uuid's to github"
     git add content/structure.xml
     git config --global user.name "CircleCI"
@@ -53,7 +57,8 @@ echo "debug ==> $CURRICULA_FOLDER/${CODE}/${VERSION}/spliced.xml"
 exit 1
 
 #copy the curriculum.xml file if a new one was created
-if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]; then
+if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]
+then
     echo "Updating spliced.xml on ${S3SERVER}"
     s3cmd del ${S3SERVER}/curricula/${SECRET_PATH_KEY}/${CODE}/${VERSION}/spliced.xml
     s3cmd put ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ${S3SERVER}/curricula/${SECRET_PATH_KEY}/${CODE}/${VERSION}/spliced.xml
@@ -63,10 +68,12 @@ fi
 #start by deleting all the files in the assets bucket
 #this is weirdly complicated, you can't just use a wildcard
 #instead, we create an empty directory, sync against it, then delete the empty directory
-if [ "`ls -A ${CURRICULA_FOLDER}/${CODE}/${VERSION}/assets`" ]; then
+if [ "`ls -A ${CURRICULA_FOLDER}/${CODE}/${VERSION}/assets`" ]
+then
     echo "Updating assets on ${S3SERVER}"
     mkdir empty_directory
     s3cmd sync --recursive --delete-removed --force empty_directory ${S3SERVER}/curricula/${SECRET_PATH_KEY}/${CODE}/${VERSION}/assets
     rm -rf empty_directory
     s3cmd put --recursive ${CURRICULA_FOLDER}/${CODE}/${VERSION}/assets/* ${S3SERVER}/curricula/${SECRET_PATH_KEY}/${CODE}/${VERSION}/assets/
 fi
+
