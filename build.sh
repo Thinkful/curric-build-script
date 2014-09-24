4f812737c601bebc1124a13394481e478ff606c0
Olex Ponomarenko <tholex@gmail.com>
2014-09-24 14:27:06 -0400

    Put the thinkdowns in order

--- build.sh
+++ build.sh
@@ -4,15 +4,20 @@
 git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/Thinkful/thinkdown
 git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/Thinkful/eagle-flavored-thinkdown thinkdown2
 npm install -g gulp
+echo "****** Installing legacy thinkdown ******"
 cd thinkdown
 npm install
 npm link
 cd ..
+echo "****** Running thinkdown (one) ******"
+thinkdown --curric=${CURRICULA_FOLDER}
+
+echo "****** Installing thinkdown2 ******"
 cd thinkdown2
 npm install
 npm link
 cd ..
-thinkdown --curric=${CURRICULA_FOLDER}
+echo "****** Running thinkdown2 ******"
 thinkdown2 --build=${CURRICULA_FOLDER}/thinkdown2/${CODE}/${VERSION}
 
 if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]
