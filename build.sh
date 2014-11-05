208178d59a29a50e7cb16917f3cb1faab85787d4
Evan Schneider <evanrs@gmail.com>
2014-11-04 21:23:01 -0500

    Kills thinkdown1

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #! /usr/bin/env bash
 
 #run thinkdown
-git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/Thinkful/thinkdown
 git clone https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/Thinkful/eagle-flavored-thinkdown thinkdown2
 npm install -g gulp
-echo "****** Installing legacy thinkdown ******"
-cd thinkdown
-npm install
-npm link
-cd ..
 
 echo "****** Installing thinkdown2 ******"
 cd thinkdown2
@@ -16,17 +10,14 @@ npm install
 npm link
 cd ..
 
-echo "****** Running thinkdown (Legacy) ******"
-thinkdown --curric=${CURRICULA_FOLDER}
-
 echo "****** Running thinkdown2 ******"
 thinkdown2 --build=${CURRICULA_FOLDER}/thinkdown2/${CODE}/${VERSION}
 
-if [ -e ${CURRICULA_FOLDER}/${CODE}/${VERSION}/spliced.xml ]
+if [ -e ${CURRICULA_FOLDER}/thinkdown2/${CODE}/${VERSION}/curriculum.json ]
 then
-    echo "Thinkdown completed, spliced.xml exists."
+    echo "Thinkdown completed, curriculum.json exists."
 else
-    echo "Thinkdown exited, but seems to have failed, spliced.xml not found."
+    echo "Thinkdown exited, it has failed, curriculum.json not found."
     exit 1
 fi
 
